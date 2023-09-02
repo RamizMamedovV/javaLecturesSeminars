1bdf866e343965effa63766a0712992767d6a351
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-09-02 18:47:06 +0300

    added class Lecture1

--- src/main/java/lecture1/Lec1.java
+++ src/main/java/lecture1/Lec1.java
@@ -1,2 +1,33 @@
-package lecture1;public class Lec1 {
+package lecture1;
+
+
+import java.io.*;
+
+public class Lec1 {
+    public static void main(String[] args) {
+//                  false = нужно ли дописывать иди перезаписать = new
+        try (FileWriter fw = new FileWriter("file.txt", true)) {
+            fw.write("line1");
+            fw.write('\n');
+            fw.write('4');
+            fw.write('\n');
+            fw.write("line3");
+            fw.flush();
+        } catch (IOException e) {
+            //System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+            String str;
+            while ((str = br.readLine()) != null) {
+                System.out.printf("== %s ==\n", str);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
