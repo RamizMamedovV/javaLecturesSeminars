dec5ce73c16775d67b014186ffc8fe599617dc50
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-09-28 07:51:43 +0300

    algorithmsAndStructures.algorithms.lectures.lec002; added SearchAlgorithms: findValue and binarySearch

---
+++ src/main/java/algorithmsAndStructures/algorithms/lectures/lec002/SearchAlgorithm.java
@@ -0,0 +1,40 @@
+package algorithmsAndStructures.algorithms.lectures.lec002;
+
+public class SearchAlgorithm {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        Sort.createArray(array, 0, 10);
+        Sort.printArray(array);
+        System.out.println(findValue(array, 5));
+        Sort.selectedSort(array);
+        System.out.print("sorted array = ");
+        Sort.printArray(array);
+        System.out.println(binarySearch(array, 5));
+
+    }
+
+    public static int findValue(int[] arr, int val) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == val) return i;
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] array, int val) {
+        return binarySearch(array,val, 0, array.length-1);
+    }
+
+    private static int binarySearch(int[] arr, int val, int start, int end) {
+        int middle;
+        if(start > end) return  -1;
+        else middle = (start + end) / 2;
+
+        if (arr[middle] == val) {
+            return middle;
+        } else if (arr[middle] > val) {
+            return binarySearch(arr, val, start, middle -1);
+        } else if (arr[middle] < val){
+            return binarySearch(arr, val, middle + 1, end);
+        } else return -1;
+    }
+}
