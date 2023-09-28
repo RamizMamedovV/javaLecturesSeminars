bf2c919d229d01d04a3996d1b9244b555a55cb1f
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-09-28 19:41:53 +0300

    added MyLibrary quickSort

---
+++ src/main/java/sem2_homework/HeapSort.java
@@ -0,0 +1,47 @@
+package sem2_homework;
+
+import myLibrary.Create_Int_Array;
+import myLibrary.Print_IntArray;
+
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] array = Create_Int_Array.createIntArray(10, 0, 10);
+        Print_IntArray.printArray(array);
+        heapSort(array);
+        Print_IntArray.printArray(array);
+    }
+
+    public static void heapSort(int[] array) {
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            heapify(array, array.length, i);
+        }
+
+        for (int i = array.length-1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
+    }
+
+    private static void heapify(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex;
+        int left = 2 * rootIndex + 1;
+        int right = 2 * rootIndex + 2;
+
+        if (left < heapSize && array[left] > array[largest])
+            largest = left;
+
+        if (right < heapSize && array[right] > array[largest])
+            largest = right;
+
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+
+            heapify(array, heapSize, largest);
+        }
+    }
+}
