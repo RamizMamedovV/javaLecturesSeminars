e6f0fefa400c5efe021d33f4b21a93dc9204232b
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-09-29 09:51:12 +0300

    added example lec003 abstract

---
+++ src/main/java/introductionOOP/Lectures/lec003/Worker.java
@@ -0,0 +1,23 @@
+package introductionOOP.Lectures.lec003;
+
+public class Worker {
+    public String firstName;
+    public String lastName;
+    public int age;
+    public int salary;
+
+    public Worker(String firstName,
+                  String lastName,
+                  int age,
+                  int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String fullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+}
