99fa4f9eb48d8049cbe90c6b3a86865af54dad22
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-10-03 14:35:34 +0300

    added myLibrary  getSurname, getName

--- src/main/java/myLibrary/GetStringRandomNameSurname.java
+++ src/main/java/myLibrary/GetStringRandomNameSurname.java
@@ -15,4 +15,28 @@ public class GetStringRandomNameSurname {
         }
         return str[0];
     }
+
+    public static String getName() {
+        Random rnd = new Random();
+        String[] str;
+        int i = rnd.nextInt(10);
+        if (i % 2 == 0) {
+            str = CreateArrayMenFemaleFirstLastNames.getFemaleNames(1);
+        } else {
+            str = CreateArrayMenFemaleFirstLastNames.getMenNames(1);
+        }
+        return str[0];
+    }
+
+    public static String getSurname() {
+        Random rnd = new Random();
+        String[] str;
+        int i = rnd.nextInt(10);
+        if (i % 2 == 0) {
+            str = CreateArrayMenFemaleFirstLastNames.getFemaleSurnames(1);
+        } else {
+            str = CreateArrayMenFemaleFirstLastNames.getMenSurnames(1);
+        }
+        return str[0];
+    }
 }
