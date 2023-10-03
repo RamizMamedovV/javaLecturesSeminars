9d9618229d70fd3bf7c2aa351ee0f05345880cc6
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-10-03 13:54:24 +0300

    added myLabrary  static Strings names, surnames for men and women

--- src/main/java/myLibrary/CreateArrayMenFemaleFirstLastNames.java
+++ src/main/java/myLibrary/CreateArrayMenFemaleFirstLastNames.java
@@ -5,45 +5,49 @@ import java.util.Random;
 public class CreateArrayMenFemaleFirstLastNames {
 
     private static final Random random = new Random();
+    public static final String[] MenSurnames = {"Иванов", "Петров", "Смирнов", "Сидоров",
+            "Кузнецов", "Васильев", "Козлов", "Лебедев", "Морозов",
+            "Новиков", "Павлов", "Соколов", "Михайлов", "Федоров", "Жуков"};
+
+    public static final String[] FemaleSurnames = {"Иванова", "Петрова", "Смирнова", "Сидорова",
+            "Кузнецова", "Васильева", "Козлова", "Лебедева", "Морозова",
+            "Новикова", "Павлова", "Соколова", "Михайлова", "Федорова", "Жукова"};
+    public static final String[] menNames = {"Александр", "Максим", "Дмитрий", "Игорь",
+            "Андрей", "Сергей", "Владимир", "Евгений", "Алексей", "Глеб"};
+    public static final String[] femaleNames = {"Анна", "Екатерина","Ольга","Елена","Мария",
+            "Татьяна", "Наталья", "Юлия", "Валентина", "Александра", "Евгения"};
+
+    // region method getMenSurnames
     public static String[] getMenSurnames(int count) {
-        String[] names = new String[] {"Иванов", "Петров", "Смирнов", "Сидоров",
-                "Кузнецов", "Васильев", "Козлов", "Лебедев", "Морозов",
-                "Новиков", "Павлов", "Соколов", "Михайлов", "Федоров", "Жуков"};
-        //Random random = new Random();
         String[] res = new String[count];
         for (int i = 0; i < count; i++) {
-            res[i] = names[random.nextInt(15)];
+            res[i] = MenSurnames[random.nextInt(15)];
         }
         return res;
     }
 
+    // end region getMenSurnames
+
     public static String[] getFemaleSurnames(int count) {
-        String[] names = new String[] {"Иванова", "Петрова", "Смирнова", "Сидорова",
-                "Кузнецова", "Васильева", "Козлова", "Лебедева", "Морозова",
-                "Новикова", "Павлова", "Соколова", "Михайлова", "Федорова", "Жукова"};
         String[] res = new String[count];
         for (int i = 0; i < count; i++) {
-            res[i] = names[random.nextInt(15)];
+            res[i] = FemaleSurnames[random.nextInt(15)];
         }
         return res;
     }
 
     public static String[] getMenNames(int count) {
-        String[] namesM = new String[] {"Александр", "Максим", "Дмитрий", "Игорь",
-                "Андрей", "Сергей", "Владимир", "Евгений", "Алексей", "Глеб"};
         String[] res = new String[count];
         for (int i = 0; i < count; i++) {
-            res[i] = namesM[random.nextInt(10)];
+            res[i] = menNames[random.nextInt(10)];
         }
         return res;
     }
 
     public static String[] getFemaleNames(int count) {
-        String[] namesF = new String[] {"Анна", "Екатерина","Ольга","Елена","Мария",
-                "Татьяна", "Наталья", "Юлия", "Валентина", "Александра", "Евгения"};
         String[] res = new String[count];
         for (int i = 0; i < count; i++) {
-            res[i] = namesF[random.nextInt(11)];
+            res[i] = femaleNames[random.nextInt(11)];
         }
         return res;
     }
